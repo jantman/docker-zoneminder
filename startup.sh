004670a1b1f7cdcbf4e109d0b80ea62c661b975a
Jason Antman <jason@jasonantman.com>
2020-11-18 17:58:35 -0500

    don't require mysql root if configured

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 
 
 set -e
+set -x
 
 #trays to fix problem with https://github.com/QuantumObject/docker-zoneminder/issues/22
 chown www-data /dev/shm
@@ -15,6 +16,12 @@ sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/
 #if ZM_DB_HOST variable is provided in container use it as is, if not left as localhost
 ZM_DB_HOST=${ZM_DB_HOST:-localhost}
 sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST|" /etc/zm/zm.conf
+MYSQL_USER=${MYSQL_USER:-zmuser}
+sed -i "s|ZM_DB_USER=zmuser|ZM_DB_USER=$MYSQL_USER|" /etc/zm/zm.conf
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-zmpass}
+sed -i "s|ZM_DB_PASS=zm|ZM_DB_PASS=$MYSQL_PASSWORD|" /etc/zm/zm.conf
+MYSQL_DATABASE=${MYSQL_DATABASE:-zm}
+sed -i "s|ZM_DB_NAME=zm|ZM_DB_NAME=$MYSQL_DATABASE|" /etc/zm/zm.conf
 #if MYSQL_ROOT_PASSWORD variable is provided in container use it as is, if not left as mysqlpsswd
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-mysqlpsswd}
 MYSQL_ROOT=${MYSQL_ROOT:-root}
@@ -24,7 +31,7 @@ if [ -v ZM_SERVER_HOST ]; then sed -i "s|#ZM_SERVER_HOST=|ZM_SERVER_HOST=${ZM_SE
 
 # Returns true once mysql can connect.
 mysql_ready() {
-  mysqladmin ping --host=$ZM_DB_HOST --user=$MYSQL_ROOT --password=$MYSQL_ROOT_PASSWORD > /dev/null 2>&1
+  mysqladmin ping --host=$ZM_DB_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD > /dev/null 2>&1
 }
 
 # Handle the zmeventnotification.ini file
@@ -43,14 +50,14 @@ if [ -f /var/cache/zoneminder/configured ]; then
           sleep 3
           echo "waiting for mysql ..."
         done
-        rm -rf /var/run/zm/* 
-	/sbin/zm.sh&
-else 
+        rm -rf /var/run/zm/*
+        /sbin/zm.sh&
+else
         #check if Directory inside of /var/cache/zoneminder are present.
         if [ ! -d /var/cache/zoneminder/events ]; then
            mkdir -p /var/cache/zoneminder/{events,images,temp,cache}
         fi
-        
+
         chown -R root:www-data /var/cache/zoneminder /etc/zm/zm.conf
         chmod -R 770 /var/cache/zoneminder /etc/zm/zm.conf
         while !(mysql_ready)
@@ -59,12 +66,12 @@ else
           echo "waiting for mysql ..."
         done
         echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST
-        mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
+        mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql
         date > /var/cache/zoneminder/dbcreated
-        #needed to fix problem with ubuntu ... and cron 
+        #needed to fix problem with ubuntu ... and cron
         update-locale
         date > /var/cache/zoneminder/configured
         zmupdate.pl
-        rm -rf /var/run/zm/* 
+        rm -rf /var/run/zm/*
         /sbin/zm.sh&
 fi
